07c617ce33c999bca6a2d3474bb6d12fd1b4e7dd
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-14 22:08:38 -0300

    correção

--- functions.js
+++ functions.js
@@ -1,11 +1,12 @@
-function pesq(){
-    let lista_pokemons = ["bulbasaur", "squirtle", "charmander"]
-    let nome_page = window.document.getElementById('ipesquisa')
-    let nome = nome_page.value
+function search(){
+    window.alert('oi');
+    let lista_pokemons = ["bulbasaur", "squirtle", "charmander"];
+    let nome_page = window.document.getElementById('ipesquisa');
+    let nome = nome_page.value;
     //teste.innerText = `teste ${lista_pokemons.includes(nome)}`
     if (lista_pokemons.includes(nome)) {
         window.location.href = `../pages-pokes/${nome.trim()}.html`
     } else {
         resp.innerText = 'ERRO'
-    }
+    };
 }
