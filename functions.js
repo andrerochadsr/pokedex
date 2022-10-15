6bc1289fa664fe27d146d19374a2cddc86d91002
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-14 22:17:34 -0300

    .

--- functions.js
+++ functions.js
@@ -1,11 +1,10 @@
 function search(){
-    window.alert('oi');
     let lista_pokemons = ["bulbasaur", "squirtle", "charmander"];
     let nome_page = window.document.getElementById('ipesquisa');
     let nome = nome_page.value;
     //teste.innerText = `teste ${lista_pokemons.includes(nome)}`
     if (lista_pokemons.includes(nome)) {
-        window.location.href = `../pages-pokes/${nome.trim()}.html`
+        window.location.href = `/pokedex/pages-pokes/${nome.trim()}.html`
     } else {
         resp.innerText = 'ERRO'
     };
