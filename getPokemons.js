15b13c123178a41ec68d0444c5b7b587d6acacd9
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-15 21:58:03 -0300

    correção da forma de extrair a URL da pagina para a URL da API

--- getPokemons.js
+++ getPokemons.js
@@ -1,20 +1,14 @@
 async function getPoke() {
     //extrai o nome do pokemon baseado na url
     const urlPoke = window.location.pathname;
-    console.log("urlPoke", urlPoke) //1 /pokedex/bulbasaur.html
     const arrayPoke = urlPoke.split('/');
-    console.log("arrayPoke", arrayPoke) //2 ['', 'pokedex', 'bulbasaur.html']
     const htmlPoke = arrayPoke[2];
-    console.log("htmlPoke", htmlPoke) //3 bulbasaur.html
-    const splitNomePoke = htmlPoke.split('.');
-    console.log("splitNomePoke", splitNomePoke) //4 ['bulbasaur', 'html']
+    const splitNomePoke = htmlPoke.split('.'); 
     const nomePoke = splitNomePoke[0];
-    console.log("nomePoke", nomePoke) //5 undefined
     // extrai o nome do pokemon baseado na url
     const url = `https://pokeapi.co/api/v2/pokemon/${nomePoke}`;
     const response = await fetch(url);
     const pokemon = await response.json();
-    console.log(pokemon)
     document.getElementById('nome-pokemon').innerHTML = pokemon['name']
     document.getElementById('numero').innerHTML += pokemon['id'];
     document.getElementById('hp').innerHTML += pokemon['stats'][0]['base_stat'];
