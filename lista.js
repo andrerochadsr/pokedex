a10512187c8bd0c1ee4b4355e1f07248b8aeacc2
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-18 20:05:16 -0300

    ajustes

--- lista.js
+++ lista.js
@@ -1,11 +1,11 @@
 async function lista() {
-    const listaPokemons = []
+    const listaPokemons = [];
     for (let i = 1 ; i <= 150; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         const response = await fetch(url);
         const pokemon = await response.json();
-        listaPokemons.push(pokemon['name'])
+        listaPokemons.push(pokemon['name']);
     }
-    sessionStorage.setItem('listaPokemons', listaPokemons)
+    sessionStorage.setItem('listaPokemons', listaPokemons);
 }
-lista()
+lista();
