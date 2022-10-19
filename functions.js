bb97f226b12436d8c7f0fb41d693d929b2e35651
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-18 22:36:19 -0300

    teste de script

--- functions.js
+++ functions.js
@@ -1,11 +1,18 @@
 async function search(){
     const listaPokesJuntos = sessionStorage.getItem('listaPokemons')
     const listaPokes = listaPokesJuntos.split(',')
-    const nome = window.document.getElementById('ipesquisa').value;
-    if (listaPokes.includes(nome) || (nome >= 1 && nome <= 898)) {
-        localStorage.setItem('pokemon', `"${nome}"`)
+    const valor = window.document.getElementById('ipesquisa').value;
+    if (valor >= 1 && valor <= 898) {
+        const id = parseInt(valor);
+        localStorage.setItem('pokemon', `"${id}"`)
         window.location.href = `pokemon.html`;
     } else {
+        if (listaPokes.includes(valor)) {
+            const nome = valor
+            localStorage.setItem('pokemon', `"${nome}"`)
+            window.location.href = `pokemon.html`;
+    } else {
         retorno.innerText = 'ERRO! Nome ou número incorreto.';
-    };
+    }
+    }
 }
