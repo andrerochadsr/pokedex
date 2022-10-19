cd00b1ac280e977e9a42be907fc895b1a4d1718e
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-10-19 19:29:52 -0300

    .

--- functions.js
+++ functions.js
@@ -3,12 +3,10 @@ function search(){
     const listaPokes = listaPokesJuntos.split(',')
     const valor = window.document.getElementById('ipesquisa').value.trim();
     if (listaPokes.includes(valor)) {
-        
         localStorage.setItem('pokemon', `"${valor}"`);
         window.location.href = `pokemon.html`;
         
     } else if (valor >= 1 && valor <= 898) {
-        
         localStorage.setItem('pokemon', `"${valor}"`);
         window.location.href = `pokemon.html`;
     } else {
