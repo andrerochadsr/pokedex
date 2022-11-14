6eb07400c63a0a85e478e6b21143d71cd8e92f67
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-11-14 10:54:11 -0300

    ajuste final (?)

--- functions.js
+++ functions.js
@@ -6,9 +6,7 @@ function search(){
         localStorage.setItem('pokemon', `"${valor}"`);
         window.location.href = `pokemon.html`;
         
-    } //else if (valor >= 1 && valor <= 898) {
-      //  localStorage.setItem('pokemon', `"${valor}"`);
-      //  window.location.href = `pokemon.html`;
+    }
     else {
     retorno.innerText = 'ERRO! Nome ou ID incorretos.'
     }
