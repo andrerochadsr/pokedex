03300875ddebfa4a1c7372d20276eae436cc1f33
Andre Rocha <99468080+andrerochadsr@users.noreply.github.com>
2022-11-14 10:50:53 -0300

    ajustes

--- functions.js
+++ functions.js
@@ -2,14 +2,14 @@ function search(){
     const listaPokesJuntos = localStorage.getItem('listaPokemons')
     const listaPokes = listaPokesJuntos.split(',')
     const valor = window.document.getElementById('ipesquisa').value.trim().toLowerCase();
-    if (listaPokes.includes(valor)) {
+    if (valor >= 1 && valor <= 898 || listaPokes.includes(valor)) {
         localStorage.setItem('pokemon', `"${valor}"`);
         window.location.href = `pokemon.html`;
         
-    } else if (valor >= 1 && valor <= 898) {
-        localStorage.setItem('pokemon', `"${valor}"`);
-        window.location.href = `pokemon.html`;
-    } else {
+    } //else if (valor >= 1 && valor <= 898) {
+      //  localStorage.setItem('pokemon', `"${valor}"`);
+      //  window.location.href = `pokemon.html`;
+    else {
     retorno.innerText = 'ERRO! Nome ou ID incorretos.'
     }
 }
