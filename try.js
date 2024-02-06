9729acde6513136ec10a542b288162e4f1483af8
Andre Rocha <andre.felipedsr@gmail.com>
2024-02-06 11:45:26 -0300

    mudança na forma de se referir aos itens dentro dos arrays
    exemplo:
    pokemon['name'] => pokemon.name

--- try.js
+++ try.js
@@ -1,7 +1,7 @@
 async function tente() {
     const idNome = document.getElementById('ipesquisa').value
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idNome}`)
-    if (response['ok'] === true) {
+    if (response.ok === true) {
         localStorage.setItem('Poke', idNome);
         window.location.href = `pokemon.html`;
     } else {
